2dc3ff0898fff3bc49cb8d132f317bc7dae96ce1
PangPang <pymehaei@gmail.com>
2026-01-04 16:47:32 +0800

    add _build_headers and request id

--- base.py
+++ base.py
@@ -3,12 +3,15 @@
 # @Author: 胖胖很瘦
 # @Date: 2025-11-10 11:07:50
 # @LastEditors: 胖胖很瘦
-# @LastEditTime: 2025-11-12 11:42:24
+# @LastEditTime: 2025-11-25 18:01:58
 
-import abc
+import abc, uuid
 from typing import Any, Optional
 
 from .apis import chat, dataset, files, documents, blocks, tags, models, sessions, feedback, textgen, workflows, app_config
+
+
+API_KEY_NAME = "DIFY_API_KEY"
 
 class BaseClient(abc.ABC):
     """Abstract base class for Dify clients.
@@ -19,8 +22,7 @@ class BaseClient(abc.ABC):
 
     Subclasses must implement the `_arequest` abstract method.
     """
-
-    def __init__(self, base_url: str, api_key: str, timeout: float = 30.0, retries: int = 3, **kwargs):
+    def __init__(self, base_url: str, api_key: dict[str, str] | str, timeout: float = 30.0, retries: int = 3, **kwargs):
         """Initialize the base client.
 
         Args:
@@ -31,21 +33,40 @@ class BaseClient(abc.ABC):
             **kwargs: Additional keyword arguments (currently unused).
         """
         self.base_url = base_url
+        if isinstance(api_key, str):
+            api_key = {API_KEY_NAME: api_key}
+
+        if API_KEY_NAME not in api_key:
+            raise ValueError(f"{API_KEY_NAME} environment variable not set")
+
         self.api_key = api_key
         self.timeout = timeout
         self.retries = retries
         self._attach_api_modules()
 
-    def _build_headers(self) -> dict:
+    def _build_headers(self, api_key_name: str = API_KEY_NAME) -> dict:
         """Build the HTTP headers for API requests.
 
         Returns:
             A dictionary containing the Authorization and Content-Type headers.
         """
+        api_key_name = api_key_name or API_KEY_NAME
+        api_key = self.api_key.get(api_key_name, None)
+        if not api_key:
+            raise ValueError(f"{api_key_name} environment variable not set")
         return {
-            "Authorization": f"Bearer {self.api_key}",
-            "Content-Type": "application/json",
+            "Authorization": f"Bearer {api_key}",
+            "Content-Type": "application/json"
         }
+
+    def _build_request_id(self) -> str:
+        """Build a unique request ID for each API request.
+
+        Returns:
+            A string representing a unique request ID.
+        """
+        return str(uuid.uuid4())
+
 
     def _build_url(self, path: str) -> str:
         """Build the full URL for an API endpoint.
